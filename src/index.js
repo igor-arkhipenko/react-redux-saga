54f64766293e87f2c9c71526c691f645ad83af05
igor-arkhipenko <i.arkhipenko@crystals.ru>
2020-08-14 19:28:59 +0300

    Connect redux to app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { rootReducer } from './redux/rootReducer'
+import { Provider } from 'react-redux'
+
+const store = createStore(rootReducer)
+
+const app = (
+    <Provider store={store}>
+      <App/>
+    </Provider>
+)
 
 render(
   <React.StrictMode>
-    <App />
+      {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
