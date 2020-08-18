21da8c57a3af7beeb63b9fecf7eae0dba80c30b2
igor-arkhipenko <i.arkhipenko@crystals.ru>
2020-08-18 17:23:54 +0300

    added alerts

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, FETCH_POST, HIDE_LOADER, SHOW_LOADER } from './types'
+import { CREATE_POST, FETCH_POST, HIDE_ALERT, HIDE_LOADER, SHOW_ALERT, SHOW_LOADER } from './types'
 
 export function createPost(post) {
     return {
@@ -19,12 +19,38 @@ export function hideLoader() {
     }
 }
 
+export function showAlert(text) {
+    return dispatch => {
+        dispatch({
+            type: SHOW_ALERT,
+            payload: text
+        })
+
+        setTimeout(() => {
+            dispatch(hideAlert())
+        }, 3000)
+    }
+}
+
+export function hideAlert() {
+    return {
+        type: HIDE_ALERT
+    }
+}
+
 export function fetchPosts() {
     return async dispatch => {
-        dispatch(showLoader())
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        const json = await response.json()
-        dispatch({ type: FETCH_POST, payload: json })
-        dispatch(hideLoader())
+        try {
+            dispatch(showLoader())
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+            const json = await response.json()
+            setTimeout(() => {
+                dispatch({ type: FETCH_POST, payload: json })
+                dispatch(hideLoader())
+            }, 500)
+        } catch (e) {
+            dispatch(showAlert('Что-то пошло не так...'))
+            dispatch(hideLoader())
+        }
     }
 }
